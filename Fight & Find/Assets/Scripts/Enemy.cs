49364c44f3be281882823770c0848b938ad4ba3d
MpHuKTo <mphukto1337@yandex.ru>
2024-10-15 15:53:42 +0500

    4el umiraet I idet na tebya
    bimbam

--- Fight & Find/Assets/Scripts/Enemy.cs
+++ Fight & Find/Assets/Scripts/Enemy.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float health;
+    [SerializeField] private float speed;
     private float currentHealth;
+    private Characters character;
+    private Rigidbody2D Rigidbody2D;
 
-    private void Awake()
+
+    protected void Awake()
     {
         currentHealth = health;
+        character = FindAnyObjectByType<Characters>();
+        Rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
 
@@ -20,6 +27,28 @@ public class Enemy : MonoBehaviour
         if (currentHealth < 0)
         {
             Debug.Log("DIE");
+
+            Destroy(gameObject);
         }
     }
+
+
+    private void Update()
+    { 
+    
+        Vector2 PlayerPos = character.transform.position;
+
+        Rigidbody2D.MovePosition(Vector2.MoveTowards(transform.position,PlayerPos,speed * Time.deltaTime));
+
+    }
+
+    protected void Move() 
+    { 
+    
+    
+
+    
+    }
+
+
 }
