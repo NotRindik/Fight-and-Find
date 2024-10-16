535fc2e882d714b5a1cefa0097982213a72e0abb
Rin <playmurka86@gmail.com>
2024-10-16 19:04:04 +0500

    ГРАФОН!!!

---
+++ Fight & Find/Assets/Scripts/WaveManager.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    public List<Enemy> enemies = new List<Enemy>();
+}
